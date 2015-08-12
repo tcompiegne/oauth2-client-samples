febeee238d7fd3d78834f51fe9a06e4ecc087a36
tcompiegne <titouan.compiegne@gmail.com>
2015-08-12 14:20:24 +0200

    Scaffold project with Yeoman Angular Generator

---
+++ angularjs-todolist/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularjsTodolistApp
+ * @description
+ * # angularjsTodolistApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularjsTodolistApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
