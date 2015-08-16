db19ad653f10de64e56e8b44d4dfe8fbc9fcbab7
tcompiegne <titouan.compiegne@gmail.com>
2015-08-16 19:05:09 +0200

    Fix unit test

--- angularjs-todolist/test/karma.conf.js
+++ angularjs-todolist/test/karma.conf.js
@@ -34,6 +34,7 @@ module.exports = function(config) {
       'bower_components/angular-xeditable/dist/js/xeditable.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
+      "app/scripts/app.js",
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
       "test/spec/**/*.js"
--- angularjs-todolist/test/spec/controllers/main.js
+++ angularjs-todolist/test/spec/controllers/main.js
@@ -17,7 +17,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(MainCtrl.awesomeThings.length).toBe(3);
+  it('user should not be logged in to the scope', function () {
+    expect(scope.isLoggedIn).toBe(false);
   });
 });
