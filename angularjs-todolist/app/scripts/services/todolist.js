b0a08aab0700b9640e818c552f6bde2cf4a64a6a
tcompiegne <titouan.compiegne@gmail.com>
2015-08-16 16:42:43 +0200

    Add license header

--- angularjs-todolist/app/scripts/services/todolist.js
+++ angularjs-todolist/app/scripts/services/todolist.js
@@ -1,3 +1,26 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Titouan COMPIEGNE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
 'use strict';
 
 /**
@@ -7,7 +30,6 @@
  * # TodoServices
  * Services of the angularjsTodolistApp
  */
-
 angular.module('angularjsTodolistApp').factory('todoService', function($http, $window){
 	var baseUrl = 'http://localhost:9001';
 	return { 
@@ -20,9 +42,9 @@ angular.module('angularjsTodolistApp').factory('todoService', function($http, $w
 					'Authorization' :	'Bearer ' + $window.sessionStorage.getItem("access_token")
  				}
 			};
-      return $http(req).then(function(result) {	
- 	    	return result.data;
-      });
+      		return $http(req).then(function(result) {	
+ 	    		return result.data;
+      		});
 		},
 
 		add: function(todo) {
@@ -35,9 +57,9 @@ angular.module('angularjsTodolistApp').factory('todoService', function($http, $w
  				},
 				data: '{ "description": "' + todo + '", "username" : "'+ $window.sessionStorage.getItem("username") +'" }'
 			};
-      return $http(req).then(function(result) {	
- 	    	return result.data;
-      });
+      		return $http(req).then(function(result) {	
+ 	    		return result.data;
+      		});
 		},
 
 		edit: function(id, todo) {
@@ -51,8 +73,8 @@ angular.module('angularjsTodolistApp').factory('todoService', function($http, $w
 				data: '{ "id": ' + id + ', "description": "'+ todo + '" }'
 			};
 			return $http(req).then(function(result) {	
- 	    	return result.data;
-      });
+ 	    		return result.data;
+      		});
 		},
 
 		remove: function(id) {
@@ -64,8 +86,8 @@ angular.module('angularjsTodolistApp').factory('todoService', function($http, $w
  				}
 			};
 			return $http(req).then(function(result) {	
- 	    	return result.data;
-      });
+ 	    		return result.data;
+      		});
 		}
 	}; 
 });
